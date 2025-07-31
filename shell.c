a40adebbd60a796bd2723b1051312f89ebd9a4f7
marpinho <mar.a.pinho@hotmail.com>
2025-07-31 16:28:35 +0100

    Fixed bug with input parsing

--- shell.c
+++ shell.c
@@ -5,16 +5,16 @@
 #include <errno.h>
 #include <limits.h>
 
-#define MAX_INPUT 1024
+#define MAX_LINE 1024
 #define MAX_ARGS 100
 
 
 int read_and_parse_input(char *argv[]) {
-    static char input[MAX_INPUT];  // static so argv points to it safely
+    static char input[MAX_LINE];  // static so argv points to it safely
     printf("sword-shell> ");
     fflush(stdout);
 
-    if (fgets(input, MAX_INPUT, stdin) == NULL) {
+    if (fgets(input, MAX_LINE, stdin) == NULL) {
         // Handle EOF (e.g., Ctrl+D)
         printf("\n");
         exit(0);
@@ -31,7 +31,6 @@ int read_and_parse_input(char *argv[]) {
     // Tokenize input
     int argc = 0;
     char *token = strtok(input, " ");     // Split input at spaces
-    char result[MAX_INPUT] = "";  // Used for caesar text
 
     while (token != NULL && argc < MAX_ARGS - 1) {
         argv[argc++] = token;
@@ -44,28 +43,29 @@ int read_and_parse_input(char *argv[]) {
     return argc;
 }
 
-handle_fib(int argc, char *argv[]){
+int handle_fib(int argc, char *argv[]){
     if (argc != 2) {
         printf("Usage: fib <n>\n");
-        return;
+        return 1;
     }
 
     int n = atoi(argv[1]);
 
     if (n < 0) {
         printf("<n> must be a non-negative integer.\n");
-        return;
+        return 1;
     }
     // Calculate Fibonacci number
 
     //printf("%d\n", result);
+    return 0;
 }
 
 
-handle_caesar(int argc, char *argv[]) {
+int handle_caesar(int argc, char *argv[]) {
     if (argc < 3) {
         printf("Usage: caesar <shift> <text>\n");
-        return;
+        return 1;
     }
 
     char *endptr;
@@ -75,13 +75,13 @@ handle_caesar(int argc, char *argv[]) {
 
     if (errno != 0 || *endptr != '\0' || shift < 0 || shift > INT_MAX) {
         printf("Invalid input. Usage: caesar <shift> <text>\n");
-        return;
+        return 1;
     }
 
     int shift_val = (int)shift;
 
     // Perform Caesar cipher encryption
-  
+    return 0;
 }
 
 int main() {
@@ -95,9 +95,9 @@ int main() {
         if (strcmp(argv[0], "exit") == 0) {
             break;
         } else if (strcmp(argv[0], "fib") == 0) {
-           // handle_fib(argc, argv);
+           handle_fib(argc, argv);
         } else if (strcmp(argv[0], "caesar") == 0) {
-            //handle_caesar(argc, argv);
+           handle_caesar(argc, argv);
         } else {
             //execute_external_command(argv);
         }
